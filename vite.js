f2dc30a6351f71ad3d9557575516e6f99646fac1
WolfeBT <wolfe.tangentllc@gmail.com>
2025-06-17 02:10:22 -0500

    update 3

---
+++ vite.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/pathfinder-sheet/', // <-- IMPORTANT: Make sure this matches your repository name
+})
